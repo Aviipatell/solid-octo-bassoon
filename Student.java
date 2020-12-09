f8a4aee15050dc2bfb0950f4f1b632b79569f190
Avi Patel <avi.patel0025@gmail.com>
2020-12-08 23:48:30 -0500

    Finished adding methods to Student and Teacher classes

--- Student.java
+++ Student.java
@@ -1,3 +1,45 @@
 public class Student {
+    private String name;
+    private int age;
+    private double average;
 
+    public Student() {
+
+    }
+
+    public Student(String name, int age, double average) {
+        this.name = name;
+        this.age = age;
+        this.average = average;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAverage(double average) {
+        this.average = average;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public double getAverage() {
+        return this.average;
+    }
+
+    // Specific Methods
+
+    public String toString() {
+        return "Student Name: " + getName() + "\nAge: " + getAge() + "\nAverage: " + getAverage();
+    }
 }
--- Teacher.java
+++ Teacher.java
@@ -1,3 +1,73 @@
 public class Teacher {
+    private String fName;
+    private String lName;
+    private String title;
+    private String field;
+    private int age;
 
+    public Teacher() {
+
+    }
+
+    public Teacher(String fName, String lName, String title, String field, int age) {
+        this.fName = fName;
+        this.lName = lName;
+        this.title = title;
+        this.field = field;
+        this.age = age;
+    }
+
+    public void setFName(String fName) {
+        this.fName = fName;
+    }
+
+    public void setLName(String lName) {
+        this.lName = lName;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFName() {
+        return this.fName;
+    }
+
+    public String getLName() {
+        return this.lName;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getField() {
+        return this.field;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    // Specific Methods
+
+    public String getName() {
+        return getFName() + " " + getLName();
+    }
+
+    public String getProfessionalTitle() {
+        return title + ". " + getLName();
+    }
+
+    public String toString() {
+        return "Teacher Name: " + getName() + "\nField: " + getField() + "\nAge: " + getAge();
+    }
 }
